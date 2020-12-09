d82c7ad4663c1f3073a504374716bf4340cb995c
mchowdhury <muz.c98@gmail.com>
2020-12-09 02:26:31 -0500

    working iwth nba package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./styles/index.scss";
 import "./styles/navbar.scss";
 import "./styles/players.scss";
+
+const nba = require('nba-api-client');
+
+console.log(nba)
+
+const jh = nba.getPlayerID("James Harden");
+
+console.log(jh)
+
+// nba.playerCareerStats({ PlayerID: 201935 }).then(data => {
+//     console.log(data).catch(err => console.log(err))
+// })
+
+const all = nba.allPlayersList().then(data => {
+    console.log(data)
+})
+console.log(all)
